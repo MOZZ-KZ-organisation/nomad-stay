95ca29d82cdb908013c58f951814e13fbc20220c
togzhanzhakhani <to7zhan@gmail.com>
2025-10-14 17:22:44 +0500

    asaфы

--- resources/views/vendor/voyager/rooms/images.blade.php
+++ resources/views/vendor/voyager/rooms/images.blade.php
@@ -1,7 +1,7 @@
 @if(!empty($data->images))
     <div style="display:flex; gap:5px; flex-wrap:wrap;">
         @foreach($data->images as $img)
-            <img src="{{ $img }}" width="100" height="70" style="object-fit:cover; border-radius:8px;">
+            <img src="{{ Voyager::image($img->path) }}" width="100" height="70" style="object-fit:cover; border-radius:8px;">
         @endforeach
     </div>
 @else
