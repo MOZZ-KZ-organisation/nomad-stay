ed952f6665f72c469e56db2342c7932429a2a56d
togzhanzhakhani <to7zhan@gmail.com>
2025-10-14 15:00:17 +0500

    images

---
+++ resources/views/vendor/voyager/rooms/images.blade.php
@@ -0,0 +1,9 @@
+@if($dataTypeContent->images->count())
+    <div style="display:flex; gap:5px; flex-wrap:wrap;">
+        @foreach($dataTypeContent->images as $img)
+            <img src="{{ $img->path }}" width="100" height="70" style="object-fit:cover; border-radius:8px;">
+        @endforeach
+    </div>
+@else
+    <span class="text-muted">Нет фото</span>
+@endif
