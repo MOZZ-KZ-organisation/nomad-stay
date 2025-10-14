51c831f1f91e6dfbb66abc1c905d821ac6c65573
togzhanzhakhani <to7zhan@gmail.com>
2025-10-14 15:11:32 +0500

    images

--- resources/views/vendor/voyager/rooms/images.blade.php
+++ resources/views/vendor/voyager/rooms/images.blade.php
@@ -1,6 +1,6 @@
-@if($dataTypeContent->images->count())
+@if($data->images->count())
     <div style="display:flex; gap:5px; flex-wrap:wrap;">
-        @foreach($dataTypeContent->images as $img)
+        @foreach($data->images as $img)
             <img src="{{ $img->path }}" width="100" height="70" style="object-fit:cover; border-radius:8px;">
         @endforeach
     </div>
