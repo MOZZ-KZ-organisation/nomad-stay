0efef0cecc01e1d7151948ce8298d8bcf0504720
togzhanzhakhani <to7zhan@gmail.com>
2025-10-16 19:27:14 +0500

    kmkm

--- resources/views/vendor/voyager/hotels/rooms.blade.php
+++ resources/views/vendor/voyager/hotels/rooms.blade.php
@@ -1,11 +1,11 @@
 @php
     $roomTitles = $data->rooms->pluck('title')->toArray();
     $titlesString = implode(', ', $roomTitles);
-    $shortText = \Illuminate\Support\Str::limit($titlesString, 25);
+    $shortText = \Illuminate\Support\Str::limit($titlesString, 30);
 @endphp
 @if($data->rooms->count())
     <a href="{{ route('voyager.rooms.index', ['hotel_id' => $data->id]) }}"
-       style="text-decoration:none; color:#18506b;">
+       style="text-decoration:none; color:#2aa0ad;" onmouseover="this.style.color='#238994';">
         <p style="margin:0;">{{ $shortText }}</p>
     </a>
 @else
