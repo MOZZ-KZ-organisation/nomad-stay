19869038ba31d311c97f421b46fca5cb6221ac68
togzhanzhakhani <to7zhan@gmail.com>
2025-10-16 19:19:19 +0500

    kmkm

--- resources/views/vendor/voyager/hotels/rooms.blade.php
+++ resources/views/vendor/voyager/hotels/rooms.blade.php
@@ -1,12 +1,14 @@
-@php
-    $count = $data->rooms->count();
-    $url = route('voyager.rooms.index', ['hotel_id' => $data->id]);
-@endphp
-
-@if($count > 0)
-    <a href="{{ $url }}" class="text-primary">
-        {{ $count }} {{ Str::plural('номер', $count) }}
-    </a>
+@if($data->rooms->count())
+    <div style="display:flex; flex-direction:column; gap:4px;">
+        @foreach($data->rooms as $room)
+            <a href="{{ route('voyager.rooms.index', ['hotel_id' => $data->id]) }}"
+               style="text-decoration:none; color:#1f2937;">
+                <p style="margin:0;">
+                    {{ \Illuminate\Support\Str::limit($room->title, 30) }}
+                </p>
+            </a>
+        @endforeach
+    </div>
 @else
     <span class="text-muted">Нет номеров</span>
 @endif
