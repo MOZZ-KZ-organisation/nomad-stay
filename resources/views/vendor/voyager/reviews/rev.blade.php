d729b1d5ec3b99e352f9a2c849ff8b9306186199
togzhanzhakhani <to7zhan@gmail.com>
2025-10-22 11:26:28 +0500

    review

--- resources/views/vendor/voyager/reviews/rev.blade.php
+++ resources/views/vendor/voyager/reviews/rev.blade.php
@@ -7,9 +7,9 @@
         <a href="{{ route('voyager.reviews.show', $booking->review->id) }}" 
            class="text-primary" 
            style="font-weight:500; text-decoration:none;">
-            ⭐{{ $booking->review->rating }}/10 — {{ Str::limit($booking->review->comment, 50) }}
+            ⭐ {{ $booking->review->rating }}/10 — {{ Str::limit($booking->review->comment, 50) }}
         </a>
-        <small class="text-muted pull-right">
+        <small class="text-muted" style="text-align: right">
             {{ $booking->review->created_at->format('d.m.Y') }}
         </small>
     </div>
