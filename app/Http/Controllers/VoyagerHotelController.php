de88417563cb2a7bc4b79c8af84ca00ea6b2f55b
Chloe Edelweiss <to7zhan@gmail.com>
2026-05-19 14:19:08 +0500

    manager

--- app/Http/Controllers/VoyagerHotelController.php
+++ app/Http/Controllers/VoyagerHotelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
 class VoyagerHotelController extends VoyagerBaseController
 {
